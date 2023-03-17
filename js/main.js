7eabe998c2e14e1b276f72828c3b06f4191f85db
Lina Kaval <kavalla@mail.uc.edu>
2023-03-17 15:01:44 -0400

    added helper functions in main

--- js/main.js
+++ js/main.js
@@ -49,24 +49,112 @@ d3.dsv("|","/data/cincy311_cleaned.tsv")
     console.log(error);
 });
 
-function updateMapMarkerColor(val){
-  if (val == "color_callType"){
-    //leafletMap.colorVar = "service_code";
-    //TODO figure out how to group categories - currently like 900 (ahhhh)
-    //leafletMap.updateVis();
+function serviceNameCategories(d){
+  
+  //cat1 - Accessibility
+  cat1Keys = ["ada compliant, city of cinti", "curb ramp, new/enhance", "handrails, repair", "request an accomodation, coc",
+  "signal, audible signal repair", "sidewalk, obstructions", "general accessibility"]
+  
+  //cat2 - Public Health
+  cat2Keys = ["mold", "roach", "mice", "rats", "animal", "mosquitoes", "flea", 
+  "food borne", "food service", "bed bugs", "racoon", "bats", "COVID-19", "rodent", "insect", "wasp, bee hive removal",
+  "food operation"]
+
+  //cat3 - Transportation & Engineering
+  cat3Keys = ["speed humps", "sidewalk", "pavement markings", "traffic island repair", "bike rack", "street", "odot", 
+  "misc traffc study cnt/accident", "encroachment", "parking meter", "light", "sign", "sunken area, repair",
+  "bridge", "default, dote", "wall, repair problem near str", "benches, repair/remove row", "constructn, build w/o permit",
+  "other rsp permit request", "dote", "guardrail", "build, permit vio dur const er", "contruct/contract complnt row"]
+
+  //cat4 - Public Services
+  cat4Keys = ["damage claim - trod", "beautification request", "street sweeping", "street cleaning", "yard waste", 
+  "recycling", "dumping", "trash cart", "graffiti", "corner can", "dead animal", "unsanitary condtn", "park graffiti", 
+  "service complaint", "row tire dumping", "row furniture/trash dumping", "dead animal", "curbs", "fence", 
+  "pothole", "landslide", "slippery streets", "steps", "barricade", "bikeway", "litter", "general repair", 
+  "spec collectn", "special collection", "nod", "vehicle", "open burning", "trash, ", "trash can, condemned",
+  "dumpster, overflow row haz", "bicycle, abandoned"]
+
+  //cat5 - Police
+  cat5Keys = ["grass front yd res", "police", "default, parking", "parking, prob at com facility", "bicyclist incident report"] 
+
+  //cat6 - Buildings and Inspections
+  cat6Keys = ["housing", "land/use", "construction signage", "construction", "life safety 6month inspection", 
+  "annual - c annual insp", "elevator", "special fire inspection",
+  "restaurant consult, new lic", "carbon monoxide test, req", "food operation, request gen", 
+  "relocation request", "tree", "tall grass/weeds", "weeds", "zoning", 
+  "fire prevention inspec request", "healthy homes, inspection", "inspection", "lot vacant", "req for insp", "fire door"]
+
+  //cat7 - City Admin
+  cat7Keys = ["constituent affairs inquiry", "default, city", "missing property, collections", "service recognition", 
+  "service compliment", "information request", "home ownership survey", "relocation survey request"]
+
+  //cat8 - Sewer and water
+  cat8Keys = ["sewage", "septic", "manhole cvr/sewer lid", "inlets", "water", "fire hydrant, repair", "sewer", "default, msd",
+  "default, cww", "restoration, repair cww", "customer inquiry, gcww"]
+
+  //cat9 - Schools, parks, recreation
+  cat9Keys = ["playground equipment problem", "school, bathroom stalls, soap", "park facility problem", "school", 
+  "default, parks", "default, recreation", "hiking trail issue", "drinking fountain problem", "park trash can overflowing",
+  "park light issues"]
+
+  //cat10 - Rentals
+  cat10Keys = ["building", "plumbing, defective", "smoke detector, missing/damagd", "unsanitary living conditions", 
+  "short term rental", "heat, no heat hazard"]
+
+  let str = d.service_name.toLowerCase()
+  // Checks whether an element is within the cat1Keys
+  const dataIncludes = (element) => str.includes(element);
+  if (cat1Keys.some(dataIncludes)){
+    return "Accessibility";
   }
-  else if (val == "color_timeBetween"){
-    //TODO finish
+  else if (cat2Keys.some(dataIncludes)){
+    return "Public Health";
   }
-  else if (val == "color_daysInYear"){
-    //TODO finish
+  else if (cat3Keys.some(dataIncludes)){
+    return "Transportation & Engineering";
   }
-  else if (val == "color_publicAgency"){
-    //leafletMap.colorVar = "AGENCY_RESPONSIBLE";
-    //TODO figure out how to group categories - currently like 900 (ahhhh)
-    //leafletMap.updateVis();
+  else if (cat4Keys.some(dataIncludes)){
+    return "Public Services";
   }
+  else if (cat5Keys.some(dataIncludes)){
+    return "Police";
+  }
+  else if (cat6Keys.some(dataIncludes)){
+    return "Buildings and Inspections";
+  }
+  else if (cat7Keys.some(dataIncludes)){
+    return "City Admin";
+  }
+  else if (cat8Keys.some(dataIncludes)){
+    return "Sewer and water";
+  }
+  else if (cat9Keys.some(dataIncludes)){
+    return "Schools, parks, recreation";
+  }
+  else if (cat10Keys.some(dataIncludes)){
+    return "Rentals";
+  }
+  else{
+    return "Other";
+  }
+  
 }
+
+function agencyResponsibleOther(d){
+  //console.log(d.agency_responsible)
+  //let agencies = ["Fire Dept", "Cin Water Works", "Park Department", "Police Department", "City Manager's Office", "Dept of Trans and Eng", "Cinc Health Dept", "Cinc Building Dept", "Public Services"];
+  let agencies_other = ["Law Department", "Community Development", "Metropolitan Sewer", "Cincinnati Recreation", "Enterprise Services", "Regional Computer Center", "Treasury Department"];
+  if (agencies_other.includes(d.agency_responsible)){
+    return "Other";
+  }
+  return d.agency_responsible
+}
+
+function updateMapMarkerColor(val){
+  leafletMap.colorCol = val;
+  leafletMap.updateVis();
+}
+
 
 function updateMapBackground(val){
   if (val == "default"){
