ed442750e1e33b997ad7e790f1394789f8f0bbf3
Lina Kaval <kavalla@mail.uc.edu>
2023-03-31 00:12:46 -0400

    fixed word cloud template

--- js/wordCloud.js
+++ js/wordCloud.js
@@ -6,11 +6,11 @@ class WordCloud{
    */
   constructor(_config, _data, ) {
     this.config = {
-    parentElement: _config.parentElement,
-    containerWidth: _config.containerWidth || 800,
-    containerHeight: _config.containerHeight || 400,
-    margin: _config.margin || {top: 30, right: 30, bottom: 100, left: 80},
-    tooltipPadding: _config.tooltipPadding || 15
+      parentElement: _config.parentElement,
+      containerWidth: _config.containerWidth || 800,
+      containerHeight: _config.containerHeight || 400,
+      margin: _config.margin || {top: 30, right: 30, bottom: 30, left: 30},
+      tooltipPadding: _config.tooltipPadding || 15
     }
 
     // this.data = _data;
@@ -22,49 +22,60 @@ class WordCloud{
   }
 
   initVis(){
-    this.width = this.config.containerWidth + this.config.margin.left + this.config.margin.right;
-    this.height = this.config.containerHeight + this.config.margin.top + this.config.margin.bottom;
-    this.svg = d3.select(this.config.parentElement)//.append("svg")
-      .attr("width", this.width)
-      .attr("height", this.height)
-      .append("g")
-        .attr("transform",
-            "translate(" + this.config.margin.left + "," + this.config.margin.top + ")");
+    let vis = this;
+    vis.width = vis.config.containerWidth + vis.config.margin.left + vis.config.margin.right;
+    vis.height = vis.config.containerHeight + vis.config.margin.top + vis.config.margin.bottom;
 
+    // append the svg object to the body of the page
+    vis.svg = d3.select(vis.config.parentElement)
+      .append("svg")
+        .attr("width", vis.width + vis.config.margin.left + vis.config.margin.right)
+        .attr("height", vis.height + vis.config.margin.top + vis.config.margin.bottom)
+        .append("g")
+        .attr("transform",
+              "translate(" + vis.config.margin.left + "," + vis.config.margin.top + ")");
+  
     this.updateVis();
   }
 
   updateVis(){
-    this.layout = d3.layout.cloud()
-      .size([this.width, this.height])
-      .words(this.data.map(function(d) { return {text: d.word, size:d.size}; }))
+    let vis = this;
+
+    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+    // Wordcloud features that are different from one word to the other must be here
+    vis.layout = d3.layout.cloud()
+      .size([vis.width, vis.height])
+      .words(vis.data.map(function(d) { return {text: d.word, size:d.size}; }))
       .padding(5)        //space between words
       .rotate(function() { return ~~(Math.random() * 2) * 90; })
       .fontSize(function(d) { return d.size; })      // font size of words
-      .on("end",function (words) {
-        this.svg
-          .append("g")
-            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-            .selectAll("text")
-              .data(words)
-            .enter().append("text")
-              .style("font-size", function(d) { return d.size; })
-              .style("fill", "#69b3a2")
-              .attr("text-anchor", "middle")
-              .style("font-family", "Impact")
-              .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-              })
-              .text(function(d) { return d.text; });
-      });
-    this.layout.start();
+      .on("end", draw);
+    vis.layout.start();
+
+    // This function takes the output of 'layout' above and draw the words
+    // Wordcloud features that are THE SAME from one word to the other can be here
+    function draw(words) {
+      vis.svg
+        .append("g")
+          .attr("transform", "translate(" + vis.layout.size()[0] / 2 + "," + vis.layout.size()[1] / 2 + ")")
+          .selectAll("text")
+            .data(words)
+          .enter().append("text")
+            .style("font-size", function(d) { return d.size; })
+            .style("fill", "#69b3a2")
+            .attr("text-anchor", "middle")
+            .style("font-family", "Impact")
+            .attr("transform", function(d) {
+              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+    }
+    this.renderVis();
   }
 
   renderVis(){
 
   }
-  
 }
-
 // List of words
 
